0368bd2ef8602190cf14b2aecc93ed43867c495f
Lokendra Dixit <dixitlokendra858@gmail.com>
2025-09-28 16:09:42 +0530

    Delete lib directory

--- lib/admin/utils/admin_ common_variable.dart
+++
@@ -1,8 +0,0 @@
-import 'package:wellwait_app/utils/common_variables.dart';
-
-int get adminServiceProviderId => prefs.getInt("serviceProviderIdSP") ?? -1;
-// int adminServiceProviderId = -1;
-
-String providerLocation = '';
-
-String adminSalonName = '';
